6329886bee47d22f153e83b2ce0edb5d9c0105a8
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-02-27 20:21:26 -0300

    setting git

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("Hello World!");
+        System.out.println ("and of course, hello Java!");
+        System.out.println ("annnd hello intelliJ!");
+    }
+}
