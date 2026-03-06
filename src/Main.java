6f8dfa7b3d05d250d7dd34690e8fce5e3f271662
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-03-06 17:30:05 -0300

    first real practice

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
+    public static void main (String[] args) {
+        int age = 21;
+        long receipt = 10L;
+        double monthIncome = 5000.00;
+        char gender = 'M';
+        boolean working = false;
 
-    public static void main(String[] args) {
-
-        Student student1 = new Student("Camila");
-        Student student2 = new Student("Leonardo");
-
-        student1.sayHello();
-        student2.sayHello();
-
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + monthIncome);
+        System.out.println("Gender: " + gender);
+        System.out.println("Working: " + working);
     }
 }
---
+++ src/MathOperators.java
@@ -0,0 +1,17 @@
+public class MathOperators {
+    public static void main (String[] args) {
+        double bread = 9.50;
+        double cheese = 7.00;
+        double sugar = 1.00;
+        double discount = 5.00;
+        int monthlyDays = 30;
+
+        double total = bread + cheese + sugar;
+        double totalWithDiscount = total - discount;
+        double dividedTotal = total / 2;
+        double monthlyTotal = totalWithDiscount * monthlyDays;
+
+        System.out.println("Receipt = " + monthlyTotal + "£");
+
+    }
+}
