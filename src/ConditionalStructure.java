d12992f980fdade0a810e997dd61d6a542698ee8
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-03-09 16:58:14 -0300

    Studying

--- src/ConditionalStructure.java
+++ src/ConditionalStructure.java
@@ -1,15 +1,18 @@
 public class ConditionalStructure {
     public static void main (String[] args) {
 
-//        int age = 30;
-//
-//        if (age >= 21){
-//            System.out.println("You are a adult!");
-//        }
-//        else {
-//            System.out.println("You are a kid!");
-//        }
-//
+        int age = 21;
+        boolean adult = true;
+
+        if (age >= 21){
+            System.out.println("You are a adult!");
+        }
+        else {
+            System.out.println("You are a kid!");
+        }
+
+        String message = (adult) ? "You are a adult" : "You are a kid";
+
 
 //        boolean raining = true;
 //
@@ -21,18 +24,17 @@ public class ConditionalStructure {
 //            System.out.println("We need to take umbrella");
 //        }
 
-        boolean haveMoney = false;
-        boolean haveCard = true;
 
-        if (haveMoney && haveCard){
-            System.out.println("We can go to a restaurant");
-        } else if (haveMoney || haveCard) {
-            System.out.println("We can order a delivery");
-        }
-        else {
-            System.out.println("We can't do anything");
-        }
-
-
+//        boolean haveMoney = false;
+//        boolean haveCard = true;
+//
+//        if (haveMoney && haveCard){
+//            System.out.println("We can go to a restaurant");
+//        } else if (haveMoney || haveCard) {
+//            System.out.println("We can order a delivery");
+//        }
+//        else {
+//            System.out.println("We can't do anything");
+//        }
     }
 }
